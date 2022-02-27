af233f6ed917a64b418697700663f6b24069d87c
Zahra0417 <99888477+Zahra0417@users.noreply.github.com>
2022-02-27 22:22:01 +0330

    Update 2.py

--- 2.py
+++ 2.py
@@ -12,8 +12,7 @@ x
 x
 2
 x + 1
-Traceback (most recent call last):
-  File "main.py", line 13, in <module>
+ File "main.py", line 13, in <module>
     print('x'+1)
 TypeError: can only concatenate str (not "int") to str
 """
