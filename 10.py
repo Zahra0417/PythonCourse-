2bd0ea887a15809cd4004472d80b3c0054b62d25
Zahra0417 <99888477+Zahra0417@users.noreply.github.com>
2022-03-12 11:54:32 +0330

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,3 +1,4 @@
+# Solve the exercise in two ways
 MAX = 20
 n = 1
 factor = 1
@@ -11,3 +12,21 @@ while n <= MAX:
     n+=1        
             
 print("reached 20")
+
+
+........................................................
+
+
+
+MAX = 20
+n = 1
+while n <= MAX:
+    factor = 1 
+    print(end=str(n)+':')
+    while factor <=n:
+        if n % factor == 0:
+            print(factor , end = ' ')
+            factor += 1 
+            print()
+            n += 1 
+            break 
