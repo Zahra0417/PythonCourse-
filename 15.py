dd6df120bfa74de5d857582bf47b0ff92fefc8e4
Zahra0417 <99888477+Zahra0417@users.noreply.github.com>
2022-03-27 17:17:02 +0430

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,33 @@
+#sin x with function
+import math 
+def sin(x,n):
+    sine=0
+    for i in range(n):
+        sign=math.pow(-1,i)
+        pi=math.pi 
+        a=x*(pi/180)
+        sine=sine+(sign*(a**(2.0*i))/math.factorial(2*i+1))
+    return sine
+    
+x=float(input('Enter The value of Degrees:'))
+n=int(input('Enter The number of terms:'))
+print(sin(x,n))
+
+.
+.
+.
+
+#sinx with out function 
+import math
+
+x = float(input('Enter The value of Degrees:'))
+n = int(input('Enter The number of terms:'))
+
+sine = 0
+for i in range(n):
+    sign = math.pow(-1, i)
+    pi = math.pi
+    a = x * (pi / 180)
+    sine = sine + (sign * (a ** (2.0 * i)) / math.factorial(2 * i + 1))
+
+print(sine)
