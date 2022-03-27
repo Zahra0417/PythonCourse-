412f2fd65a1b878a6037610340e176581318a85c
Zahra0417 <99888477+Zahra0417@users.noreply.github.com>
2022-03-27 22:07:33 +0430

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,6 +1,7 @@
 text = input("Enter a text:")
+key = int(input("Enter key:"))
 encrypted = " "
 for letter in text:
-    encrypted += chr(ord(letter)+3)
+    encrypted += chr(ord(letter)+key)
     
 print(encrypted)
