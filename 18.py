d75f0d68c74c25bfa0659cb8ae8718dcc8b9add9
Zahra0417 <99888477+Zahra0417@users.noreply.github.com>
2022-05-04 23:05:59 +0430

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,2 +1,78 @@
 """
 Exercise 1
+answer:Yes
+
+Exercise 2
+answer:with the help of the negative index in Python ,we can access the elements of the list from the bottom.
+
+Exercise 3
+answer:list=[45,-3,16,8]
+
+Exercise 4
+answer:
+(a) lst[0]
+(b) lst[3]
+(c) 10
+(d) 29
+(e) -4
+(f) 29
+(g) 10
+(h) illegal
+
+Exercise 5
+answer:
+(a) 3
+(b) 5
+(c) 1
+(d) 5
+(e) 5
+(f) 2
+(g) 0
+(h) 3
+
+
+Exercise 6
+answer: Function len
+
+
+Exercise 7
+answer: a=[]
+
+Exercise 8
+answer:
+(a) [20,1,-34,40,-8,60,1,3]
+(b) [20,1,-34]
+(c) [-8,60,1,3]
+(d) [-8,60,1,3]
+(e) [40,-8]
+(f) [20,1,-34]
+(g) [-8,60,1,3]
+(h) [20,1,-34,40,-8,60,1,3]
+(i) [20,1,-34,40]
+(j) [1,-34,40,-8]
+(k) True
+(l) False
+(m) 8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
